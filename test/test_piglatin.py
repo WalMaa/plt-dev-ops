315f5ae8dfdb5e71a8486d69b314f878b6a954de
WalMaa <wmaatta21@student.oulu.fi>
2024-10-31 17:01:38 +0200

    translator creation

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -8,3 +8,7 @@ class TestPigLatin(unittest.TestCase):
     def test_translator_creator(self):
         translator = PigLatin("phrase")
 
+    def test_get_phrase(self):
+        translator = PigLatin("phrase")
+        self.assertEquals("phrase", translator.get_phrase())
+
