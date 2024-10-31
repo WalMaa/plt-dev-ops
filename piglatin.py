6953ea2ec7d4f3acc4f9a1f4758100a8115be624
WalMaa <wmaatta21@student.oulu.fi>
2024-10-31 17:13:50 +0200

    US1 fixed class API

--- piglatin.py
+++ piglatin.py
@@ -1,13 +1,20 @@
 class PigLatin:
 
-    phrase: str
+    __phrase: str
 
     def __init__(self, phrase: str):
-        self.phrase = phrase
+        self.__phrase = phrase
 
     def get_phrase(self) -> str:
-        return self.phrase
+        return self.__phrase
 
     def translate(self) -> str:
         pass
 
+    class PigLatinTranslator:
+
+        def __init__(self, phrase: str):
+            self.__phrase = phrase
+
+        def get_phrase(self) -> str:
+            return self.__phrase
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -1,4 +1,6 @@
 import unittest
+from idlelib.pyparse import trans
+
 from piglatin import PigLatin
 from error import PigLatinError
 
@@ -6,9 +8,10 @@ from error import PigLatinError
 class TestPigLatin(unittest.TestCase):
 
     def test_translator_creator(self):
-        translator = PigLatin("phrase")
+        translator = PigLatin.PigLatinTranslator("phrase")
+        self.assertTrue(isinstance(translator,PigLatin.PigLatinTranslator))
 
     def test_get_phrase(self):
-        translator = PigLatin("phrase")
+        translator = PigLatin.PigLatinTranslator("phrase")
         self.assertEquals("phrase", translator.get_phrase())
 
